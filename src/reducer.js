3d74c3e9068f8d02ba409d237d58a3f05447f2e9
Philip Levy <philipdlevy@gmail.com>
2022-08-25 10:02:44 -0600

    commit tree 61c5e6860f0f490c32bcb9d337dcb59c7db94730

--- src/reducer.js
+++ src/reducer.js
@@ -1,3 +1,20 @@
+// function changeState(state, action) {
+//   switch (action.type) {
+//     case "counter/increment":
+//       return { count: state.count + 1 };
+//     default:
+//       return state;
+//   }
+// }
+
+// let state = { count: 0 };
+// let action = { type: "counter/increment" };
+
+// changeState(state, action);
+
+
+let state = { count: 0 };
+
 function changeState(state, action) {
   switch (action.type) {
     case "counter/increment":
@@ -7,7 +24,29 @@ function changeState(state, action) {
   }
 }
 
-let state = { count: 0 };
-let action = { type: "counter/increment" };
+function render() {
+  const app = document.querySelector("#app");
+  app.textContent = state.count;
+}
 
-changeState(state, action);
+// function dispatch(action) {
+//   state = changeState(state, action);
+//   return state;
+// }
+
+function dispatch(action) {
+  state = changeState(state, action);
+  render();
+}
+
+dispatch({ type: "counter/increment" });
+// => {count: 1}
+dispatch({ type: "counter/increment" });
+// => {count: 2}
+dispatch({ type: "counter/increment" });
+// => {count: 3}
+dispatch({ type: "counter/increment" });
+// => {count: 4}
+dispatch({ type: "counter/increment" });
+// => {count: 5}
+
